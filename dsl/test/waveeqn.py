7a69c49b986d67361e9dd97974ff7be870780c70
Steven R. Brandt <sbrandt@cct.lsu.edu>
2024-07-02 16:24:59 +0000

    There shall be only one wave eqn

--- dsl/test/waveeqn.py
+++ dsl/test/waveeqn.py
@@ -23,7 +23,9 @@ cfu.verbose = True
 def flat_metric(out: Expr, ni: Idx, nj: Idx) -> Expr:
     i = to_num(ni)
     j = to_num(nj)
-    if i == j:
+    if i == 2 or j == 2:
+        return sympify(0)
+    elif i == j:
         return sympify(1)
     else:
         return sympify(0)
@@ -33,12 +35,10 @@ def flat_metric(out: Expr, ni: Idx, nj: Idx) -> Expr:
 gf = ThornDef("TestWave", "WaveEqn")
 
 # Declare gfs
-p_t = gf.decl("p_t", [li], Centering.VVC)
-p = gf.decl("p", [li], Centering.VVC, rhs=p_t)
-p_d = gf.decl("p_d", [li, lj], Centering.VVC)
+v_t = gf.decl("v_t", [li], Centering.VVC)
+v = gf.decl("v", [li], Centering.VVC, rhs=v_t)
 u_t = gf.decl("u_t", [], Centering.VVC)
 u = gf.decl("u", [], Centering.VVC, rhs=u_t)
-u_d = gf.decl("u_d", [ui], Centering.VVC)
 
 siter2 = gf.decl("siter2", [li, lj])
 gf.add_sym(siter2[li, lj], li, lj)
@@ -57,66 +57,19 @@ ky = gf.add_param("ky", default=pi / 20, desc="The wave number in the y-directio
 gf.fill_in(g[li, lj], flat_metric)
 gf.fill_in(g[ui, uj], flat_metric)
 
-# Fill in with defaults
-gf.fill_in(p[li], lambda _, i: mkSymbol(f"pD{to_num(i)}"))
-gf.fill_in(p_t[li], lambda _, i: mkSymbol(f"p_tD{to_num(i)}"))
-
-# Fill in the deriv variables with a function call
-#
-div1 = gf.declfun("div1", True)
-divx = gf.declfun("divx", True)
-divy = gf.declfun("divy", True)
-divz = gf.declfun("divz", True)
-gf.fill_in(p_d[li, lj], lambda _, i, j: div1(p[j], i))
-gf.fill_in(u_d[li], lambda _, i: div1(u, i))
-
-
-def to_div(out: Expr, j: Idx) -> Expr:
-    n = to_num(j)
-    ret: Any
-    if n == 0:
-        ret = divx(*out.args[:-1])
-    elif n == 1:
-        ret = divy(*out.args[:-1])
-    elif n == 2:
-        ret = divz(*out.args[:-1])
-    else:
-        assert False
-    return cast(Expr, ret)
-
-
-def to_div2(out: Expr, i: Idx, j: Idx) -> Expr:
-    n = to_num(j)
-    ret: Any
-    if n == 0:
-        ret = divx(p[i])
-    elif n == 1:
-        ret = divy(p[i])
-    elif n == 2:
-        ret = divz(p[i])
-    else:
-        assert False
-    return cast(Expr, ret)
-
-
-gf.fill_in(iter1[lj], alt=div1(u, lj), f=to_div)
-gf.fill_in(siter2[li, lj], alt=div1(p[li], lj), f=to_div2)
-
 x, y, z = gf.coords()
-
-res = gf.do_subs(spd * g[ui, uj] * div1(p[lj], li))
 
 # Add the equations we want to evolve.
 fun = gf.create_function("newwave_evo", ScheduleBin.Evolve)
-fun.add_eqn(p_t[lj], spd * div1(u, lj))
-fun.add_eqn(u_t, spd * g[ui, uj] * div1(p[lj], li))
+fun.add_eqn(v_t, u)
+fun.add_eqn(u_t, spd ** 2 * g[ui, uj] * div(v, li, lj))
 print('*** ThornFunction wave_evo:')
 
 # Ensure the equations make sense
 fun.diagnose()
 
 # Perform cse
-fun.cse()
+# fun.cse()
 
 # Dump
 fun.dump()
@@ -126,16 +79,18 @@ fun.show_tensortypes()
 
 # Again for wave_init
 fun = gf.create_function("newwave_init", ScheduleBin.Init)
-fun.add_eqn(u, sin(kx * x) * sin(ky * y))
-fun.add_eqn(p[l0], kx * cos(kx * x) * sin(ky * y))
-fun.add_eqn(p[l1], ky * sin(kx * x) * cos(ky * y))
-fun.add_eqn(p[l2], sympify(0))
-
+fun.add_eqn(v, sin(kx * x) * sin(ky * y))
+fun.add_eqn(u, sympify(0))  # kx**2 * ky**2 * sin(kx * x) * sin(ky * y))
 print('*** ThornFunction wave_init:')
 fun.diagnose()
 fun.cse()
 fun.diagnose()
 fun.dump()
 fun.show_tensortypes()
+
+fun = gf.create_function("refine", ScheduleBin.EstimateError)
+regrid_error = gf.decl("regrid_error", [], Centering.CCC)
+fun.add_eqn(regrid_error, 10/((x-20)**2 + (y-20)**2))
+fun.diagnose()
 
 CppCarpetXWizard(gf).generate_thorn()
