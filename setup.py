aa1d13600c27b7580f19847903110bd132601501
Max Morris <mmor115@lsu.edu>
2024-09-23 15:44:01 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EmitCactus',
+    version='0.1.0',
+    description='DSL and toolset for creating Cactus thorns',
+    url='https://github.com/mmor115/EmitCactus',
+    author='Max Morris',
+    author_email='mmorris@cct.lsu.edu',
+    license='MIT',
+    packages=find_packages(include='EmitCactus.*'),
+    install_requires=[
+        'mypy==1.10.0',
+        'nrpy==2.0.18',
+        'sympy==1.12.1',
+        'multimethod>=1.10',
+        'numpy==2.1.0',
+        'pdoc==14.6.0'
+    ]
+)
