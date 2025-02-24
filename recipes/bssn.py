c0cf727331ac75faabe3dbdb1e9f62d15050a0e2
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-02-24 11:04:00 -0600

    BSSN: Restored bake statements on all functions

--- recipes/bssn.py
+++ recipes/bssn.py
@@ -315,7 +315,7 @@ if __name__ == "__main__":
         g_driver_eta * g_driver_B[ua]
     )
 
-    # fun.bake()
+    fun.bake()
 
     ###
     # Convert ADM to BSSN variables
@@ -344,7 +344,7 @@ if __name__ == "__main__":
         4.0 * (dtbeta[ua] - beta[ui] * div(beta[ua], li)) / (3.0 * alp)
     )
 
-    # funload.bake()
+    funload.bake()
 
     ###
     # Convert BSSN to ADM variables
@@ -362,7 +362,7 @@ if __name__ == "__main__":
     funstore.add_eqn(alp, evo_lapse)
     funstore.add_eqn(beta[ua], evo_shift[ua])
 
-    # funstore.bake()
+    funstore.bake()
 
     ###
     # Compute constraints
