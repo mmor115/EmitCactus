c27ecb0eb8436842180315cad45eb19fd4d5b6f0
Max Morris <mmor115@lsu.edu>
2025-06-06 11:46:07 -0500

    Added a --clean option to typecheck.sh

--- typecheck.sh
+++ typecheck.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-if [ ! -d "venv" ]; then
-    echo "./venv does not exist. Please set up your venv."
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+if [ "$1" == "-c" ] || [ "$1" == "--clean" ]; then
+    rm -rf "$SCRIPT_DIR/.mypy_cache/"
+fi
+
+if [ ! -d "$SCRIPT_DIR/venv" ]; then
+    echo "$SCRIPT_DIR/venv does not exist. Please set up your venv."
     exit 2
 fi
+
+cd "$SCRIPT_DIR"
 
 . ./venv/bin/activate
 
