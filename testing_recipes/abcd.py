708d991d3da71fe009572cb40b5b8f5524e83f1a
Max Morris <mmor115@lsu.edu>
2025-12-05 19:53:06 -0600

    Added recipe displaying the problem.

---
+++ testing_recipes/abcd.py
@@ -0,0 +1,32 @@
+from EmitCactus import *
+
+thorn = ThornDef('Test', 'Abcd')
+
+opts = {
+    "do_split_output_eqns": False
+}
+
+a = thorn.add_param('a', 1.0, 'a')
+b = thorn.add_param('b', 1.0, 'b')
+c = thorn.add_param('c', 1.0, 'c')
+
+o1 = thorn.decl('o1', [])
+o2 = thorn.decl('o2', [])
+
+f1 = thorn.create_function('f1', ScheduleBin.Evolve)
+f2 = thorn.create_function('f2', ScheduleBin.Evolve)
+
+f1.add_eqn(o1, a + b + c)
+f2.add_eqn(o2, a + b)
+
+f1.bake(**opts)
+f2.bake(**opts)
+
+thorn.do_global_cse()
+
+CppCarpetXWizard(
+    thorn,
+    CppCarpetXGenerator(
+        thorn
+    )
+).generate_thorn()
